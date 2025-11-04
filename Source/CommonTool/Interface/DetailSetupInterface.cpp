7a17a1d93694f1fe3e69ff5e36834471486eb10c
PiangGG <974805475@qq.com>
2025-11-04 16:25:29 +0800

    ReSet plugin

--- Source/CommonTool/Interface/DetailSetupInterface.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "DetailSetupInterface.h"
-
-// Add default functionality here for any IDetailSetupInterface functions that are not pure virtual.
